0e1889bbc90d09d44488a460689e5397c520f7ea
phonglk <phong.lekhoi@gmail.com>
2020-05-14 18:01:59 +0800

    feat: auto deploy

--- script/deploy_all_functions.sh
+++ script/deploy_all_functions.sh
@@ -1,2 +1,28 @@
+#!/usr/bin/env bash
 echo "Deploy all functions"
-aws s3 ls
+
+# Prepare
+mkdir -p ./tmp/lambda
+rm -rf ./tmp/lambda/*
+
+cd ./tmp/lambda
+
+for path in ../../src/*; do
+  FUNC=$(basename $path)
+  if [ "$FUNC" != "common" ]; then
+    echo ">Processing function $FUNC";
+    mkdir -p ./$FUNC
+    cp -r ../../src/$FUNC ./$FUNC/
+    cp -r ../../src/common ./$FUNC/
+    echo "exports.handler=require('./$FUNC/index').handler" > ./$FUNC/index.js
+
+    echo ">>Compact"
+    cd $FUNC
+    zip -r ../$FUNC.zip .
+
+    cd ..
+    echo ">>Update function"
+    aws lambda update-function-code --function-name ${FUNC} --zip-file fileb://${FUNC}.zip
+  fi
+done
+
